5fa1843c09cb243ecceaa0262b1990b0ebac1ddf
muratas <rw.satoshi.murata@gmail.com>
2019-02-15 14:12:10 +0900

    ファイルを追加

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const bs = require('browser-sync').create();
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const plumber = require('gulp-plumber');
+
+gulp.task('serve', () => {
+  bs.init({
+    server: './dest',
+    open: false,
+    port: 3030,
+    ui: {
+      port: 3031,
+    },
+  });
+
+  gulp.watch('src/**/*.scss', gulp.parallel('build-sass'));
+});
+
+gulp.task('build-sass', () => {
+  return gulp.src(['src/**/*.scss', '!src/**/_*.scss'])
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(gulp.dest('dest'));
+});
+
+gulp.task('default', gulp.parallel('serve'));
